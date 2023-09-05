70b886da9cb82fce88be87c9bbba694d1d5cb322
hp <hp@DESKTOP-GGGBJHR>
2023-09-05 13:43:28 +0530

    Add project files.

---
+++ MVC_492023/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MVC_492023.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        SqlConnection con = new SqlConnection("data source=DESKTOP-GGGBJHR//SQLEXPRESS;initial catalog=MVC_492023;integrated security=true");
+        public ActionResult EmployeeForm()
+        {
+            return View();
+        }
+
+
+        public void EmployeeData(string A, int B, int C)
+        {
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("employeeInsert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name",A);
+            cmd.Parameters.AddWithValue("@salary",B);
+            cmd.Parameters.AddWithValue("@age",C);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+     
+        }
+    }
+}
---
+++ MVC_492023/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_492023.MvcApplication" Language="C#" %>
